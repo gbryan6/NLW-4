e59c5425ecc8f2d03cee722514180b56ccfb3889
Gabriel Bryan <gbryan6@hotmail.com>
2021-02-28 02:33:16 -0300

    Faz layout do  card challenges

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -2,21 +2,48 @@ import React from "react";
 import styles from "../../styles/components/ChallengeBox.module.css";
 
 const ChallengeBox: React.FC = () => {
+  const hasActiveChallenge = true;
+
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <p>
-            Inicie um ciclo<br/> 
-            para receber desafios a<br/> 
-            serem completados
-        </p>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400xp</header>
 
-        <div className={styles.wrapperExplanation}>
-          <img src="icons/level-up.svg" alt="Level Up" />
-          <p>Complete-os e ganhe<br />
-            experiência e avance de leve.</p>
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Faça uma caminhadinha de pelomenos 50 minutos cada.</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+          </footer>
         </div>
-      </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <p>
+            Inicie um ciclo
+            <br />
+            para receber desafios a<br />
+            serem completados
+          </p>
+
+          <div className={styles.wrapperExplanation}>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            <p>
+              Complete-os e ganhe
+              <br />
+              experiência e avance de leve.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
