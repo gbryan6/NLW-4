26ef2f2f85e9b42012cafa22c7c86aaca9721e71
Gabriel Bryan <gbryan6@hotmail.com>
2021-02-27 23:52:11 -0300

    Faz layout do countdown

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -6,7 +6,10 @@ const Profile: React.FC = () => {
         <img src="https://github.com/gbryan6.png" alt="Foto de Perfil"/>
         <div>
             <strong>Gabriel Bryan</strong>
-            <p>Level 1</p>
+            <p>
+                <img src="icons/level.svg" alt="Level"/>
+            Level 1
+            </p>
         </div>
     </div>
   );
